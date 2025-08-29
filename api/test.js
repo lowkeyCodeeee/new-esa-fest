bbf69c61aa24603ac916cfcc49543f1dbb1b330a
lowkeyCodee <moisesnugal24@gmail.com>
2025-08-29 08:47:51 +0800

    added connection string for request

--- api/test.js
+++ api/test.js
@@ -1,8 +1,11 @@
 
 
+const uri = process.env.MONGO_CONNECTION_STRING;
+
+
 export default function test(req, res) {
     if(req.method === 'GET'){
-        res.status(200).json({status : 'ok', message : 'hello world'});
+        res.status(200).json({status : 'ok', message : 'hello world', uri});
     }
     res.status(400).json({message : 'Bad Request from test'});
 }
