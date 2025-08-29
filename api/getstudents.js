0140aaff6f64fd0fb380d04a012473dba3ef2a09
lowkeyCodee <moisesnugal24@gmail.com>
2025-08-29 08:53:14 +0800

    added DB connection in test

--- api/getstudents.js
+++ api/getstudents.js
@@ -1,6 +1,7 @@
 import { connectDB } from "../lib/db.js";
 import { getAllStudents } from "../lib/middleware.js";
 export default async function handler(req, res) {
+
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -8,6 +9,7 @@ export default async function handler(req, res) {
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "GET") return res.status(405).json({ message: "Method not allowed" });
 
+  
   try {
     const db = await connectDB();
     const students = await getAllStudents(db);
--- api/test.js
+++ api/test.js
@@ -1,11 +1,20 @@
 
-
+import { connectDB } from "../lib/db";
+import { getAllStudents } from "../lib/middleware";
 const uri = process.env.MONGO_CONNECTION_STRING;
 
 
-export default function test(req, res) {
+export default async function test(req, res) {
     if(req.method === 'GET'){
         res.status(200).json({status : 'ok', message : 'hello world', uri});
     }
-    res.status(400).json({message : 'Bad Request from test'});
+
+    try {
+        const db = await connectDB();
+        const students = await getAllStudents(db);
+        res.status(200).json({ studentList: students });
+      } catch (err) {
+        console.error("❌ Error in getstudents:", err); // 🔹 log full error
+        res.status(500).json({ message: err.message }); // 🔹 return real error message
+      }
 }
