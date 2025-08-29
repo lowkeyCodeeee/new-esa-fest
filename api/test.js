f3820b59b8c9925e35bc91219da1ce0c7cb4eae0
lowkeyCodee <moisesnugal24@gmail.com>
2025-08-29 09:30:42 +0800

    test imports

--- api/test.js
+++ api/test.js
@@ -7,18 +7,18 @@ const uri = process.env.MONGO_CONNECTION_STRING;
 export default async function test(req, res) {
     if (req.method === 'GET') {
         res.status(200).json({ status: 'ok', message: 'hello world', uri });
-        try {
-            const db = await connectDB();
-            // const students = await getAllStudents(db);
+        // try {
+        //     const db = await connectDB();
+        //     // const students = await getAllStudents(db);
 
-            const studentInstance = db.collection('attendance');
-            const students = await studentInstance.find().toArray();
+        //     const studentInstance = db.collection('attendance');
+        //     const students = await studentInstance.find().toArray();
 
-            res.status(200).json({ studentList: students });
-        } catch (err) {
-            console.error("❌ Error in studentIns:", err); // 🔹 log full error
-            res.status(500).json({ db: 'Error in connecting database mongodb', message: err.message }); // 🔹 return real error message
-        }
+        //     res.status(200).json({ studentList: students });
+        // } catch (err) {
+        //     console.error("❌ Error in studentIns:", err); // 🔹 log full error
+        //     res.status(500).json({ db: 'Error in connecting database mongodb', message: err.message }); // 🔹 return real error message
+        // }
     }
 
     // res.status(400).json({msg : 'Bad request'});
