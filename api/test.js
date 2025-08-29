48e416a877c56f4e9d6db90437700454cd142f5a
lowkeyCodee <moisesnugal24@gmail.com>
2025-08-29 09:26:34 +0800

    created new db instance

--- api/test.js
+++ api/test.js
@@ -1,21 +1,26 @@
 
-// import { connectDB } from "../lib/db";
+import { connectDB } from "../lib/db";
 // import { getAllStudents } from "../lib/middleware";
 const uri = process.env.MONGO_CONNECTION_STRING;
 
 
 export default async function test(req, res) {
-    if(req.method === 'GET'){
-        res.status(200).json({status : 'ok', message : 'hello world', uri});
+    if (req.method === 'GET') {
+        res.status(200).json({ status: 'ok', message: 'hello world', uri });
+        try {
+            const db = await connectDB();
+            // const students = await getAllStudents(db);
+
+            const studentInstance = db.collection('attendance');
+            const students = await studentInstance.find().toArray();
+
+            res.status(200).json({ studentList: students });
+        } catch (err) {
+            console.error("❌ Error in studentIns:", err); // 🔹 log full error
+            res.status(500).json({ db: 'Error in connecting database mongodb', message: err.message }); // 🔹 return real error message
+        }
     }
 
-    res.status(400).json({msg : 'Bad request'});
-    // try {
-    //     const db = await connectDB();
-    //     const students = await getAllStudents(db);
-    //     res.status(200).json({ studentList: students });
-    //   } catch (err) {
-    //     console.error("❌ Error in getstudents:", err); // 🔹 log full error
-    //     res.status(500).json({db :'Error in connecting database mongodb',  message: err.message }); // 🔹 return real error message
-    //   }
+    // res.status(400).json({msg : 'Bad request'});
+
 }
